7644cb799213a9fc1d8f44de4ed5273623fcd205
Zxf21 <zhao.xuefei21@outlook.com>
2020-06-30 00:15:40 +0800

    1

--- moveapp/src/views/Movie.vue
+++ moveapp/src/views/Movie.vue
@@ -2,22 +2,21 @@
   <div>
     <ul class="movie-container">
       <li class="movie-list" v-for="obj in movieList" :key="obj.id">
-        <img :src="obj.images.small" alt="" />
+        <img :src="obj.images.small" alt />
         <div class="movie-info">
           <h3>{{ obj.title }}</h3>
           <p>
-            <span v-for="cast in obj.casts" :key="cast.id"
-              >{{ cast.name }} |
-            </span>
+            <span v-for="cast in obj.casts" :key="cast.id">{{ cast.name }} |</span>
           </p>
           <p>{{obj.collect_count}}</p>
           <p>{{obj.year}}</p>
           <p>
-            <span v-for="(genre, index) in obj.genres" :key="index">{{genre}} | </span>
+            <span v-for="(genre, index) in obj.genres" :key="index">{{genre}} |</span>
           </p>
         </div>
       </li>
     </ul>
+    <img v-if="loadingShow" src="../assets/img/loading.gif" alt class="loadingImg" />
   </div>
 </template>
 
@@ -27,6 +26,7 @@ export default {
   data() {
     return {
       movieList: [],
+      loadingShow: true
     };
   },
   created() {
@@ -34,14 +34,25 @@ export default {
       .get(
         `https://bird.ioliu.cn/v1?url=https://api.douban.com/v2/movie/top250?apikey=0df993c66c0c636e29ecbb5344252a4a&start=0&count=10`
       )
-      .then((res) => {
+      .then(res => {
         console.log(res.data.subjects);
         this.movieList = res.data.subjects;
+        this.loadingShow = false;
       })
-      .catch((res) => {
+      .catch(res => {
         console.log(res);
       });
-  },
+
+    window.onscroll = () => {
+      let scrollTop = document.documentElement.scrollTop;
+      let clientHeight = document.documentElement.clientHeight;
+      let pageHeight = document.documentElement.scrollHeight;
+
+      if(scrollTop + clientHeight == pageHeight) {
+        
+      }
+    };
+  }
 };
 </script>
 
@@ -63,5 +74,13 @@ export default {
       flex-grow: 1;
     }
   }
+}
+
+.loadingImg {
+  width: 1rem;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
